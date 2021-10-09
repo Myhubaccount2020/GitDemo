f6ed25a02a2e2177cc6df5752529b14b851ba3f0
Myhubaccount2020 <mouradikhlef1979@gmail.com>
2021-10-09 00:14:24 -0400

    My first commit

---
+++ Myjavaprog.java
@@ -0,0 +1,16 @@
+class HelloWorld 
+{ 
+    // Your program begins with a call to main(). 
+    // Prints "Hello, World" to the terminal window.
+    static int a=10;
+    static String name = "steeve";
+    public static void main(String args[]) 
+    { 
+        int a = 2;
+        // String name = "steeve";
+        System.out.println("Hello, World");
+        System.out.println(a);
+        System.out.println(name);
+
+    } 
+} 
